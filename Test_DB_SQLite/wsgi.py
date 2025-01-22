8f03e03ca0027977e6fd54e3e8e81b54a434f91d
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-22 19:25:55 +0600

    upd

---
+++ Test_DB_SQLite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Test_DB_SQLite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test_DB_SQLite.settings')
+
+application = get_wsgi_application()
